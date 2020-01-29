fda12c9aacb5f35b6e7ee50c0d346b43ffd9eb82
jmorley <jmorley@bravurasolutions.com>
2020-01-29 20:15:36 +0100

    Implements a simple counter in React

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Message from "./components/Message";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet />
       <Welcome /> */}
       {/* <Greet name="Bruce" heroName="Batman">
