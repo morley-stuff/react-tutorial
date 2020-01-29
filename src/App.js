787559ac619d38dc0e38d72d893d6052e4123095
jmorley <jmorley@bravurasolutions.com>
2020-01-29 22:01:32 +0100

    Added functionality dealing with events and event handler binding

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
 import "./App.css";
-
-import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-import Message from "./components/Message";
-import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      {/* <Message /> */}
-      {/* <Greet />
-      <Welcome /> */}
-      {/* <Greet name="Bruce" heroName="Batman">
-        <p>This is children props</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman">
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" /> */}
+      <EventBind></EventBind>
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick></ClassClick> */}
     </div>
   );
 }
---
+++ src/components/EventBind.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class EventBind extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "Hello"
+    };
+    // this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  // clickHandler() {
+  //   this.setState({ message: "Goodbye" });
+  //   console.log(this);
+  // }
+
+  clickHandler = () => {
+    this.setState({
+      message: "Goodbye"
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.message}</div>
+        {/* <button onClick={this.clickHandler.bind(this)}>click</button> */}
+        <button onClick={this.clickHandler}>click</button>
+      </div>
+    );
+  }
+}
+
+export default EventBind;
