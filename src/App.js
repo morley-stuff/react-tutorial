594b3eecd32cf0edfa94ead78dfc34221235e80b
jmorley <jmorley@bravurasolutions.com>
2020-01-30 16:14:32 +0100

    [Tutorial 17] List rendering

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import ParentComponent from "./components/ParentComponent";
 import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
 
 function App() {
   return (
     <div className="App">
-      <UserGreeting></UserGreeting>
+      <NameList />
     </div>
   );
 }
